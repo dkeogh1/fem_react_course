410708ecbf962ec3a4a26186bf13baa1efd65925
dkeogh1 <danielkeogh@Daniels-MacBook-Pro.local>
2020-04-19 09:16:04 -0700

    initial course work

---
+++ adopt-me/src/App.js
@@ -0,0 +1,31 @@
+const Pet = ({name, animal, breed}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
+};
+
+
+const App = () => {
+    return React.createElement("div", { id : "something=important"}, [
+        React.createElement("h1", {}, 'Adopt Me!'),
+        React.createElement(Pet, { 
+            name: "Otis", 
+            animal: "Cat", 
+            breed: "Street"}),
+        React.createElement(Pet, { 
+            name: "Jeff", 
+            animal: "Dog", 
+            breed: "Sheperd"}),
+        React.createElement(Pet, { 
+            name: "Boink", 
+            animal: "Cat", 
+            breed: "Mixed"})
+    ]);
+};
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
